6e7aca90ef013ca7ea471f6c6f0d4c476db6c6a9
Ankit <ankitdm69@gmail.com>
2021-08-08 17:53:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react'
 import "./App.css"
 import Header from './Layout/Header';
 import Sidebar from './Layout/Sidebar';
+import Home from './Pages/Home';
 
 const App = () => {
-
-
-
-
-
-
 
   return (
 
@@ -17,159 +12,8 @@ const App = () => {
       <div className="gs-container">
         <Sidebar />
         <div className="gs-body gs-col container-fluid bg-light" id="gs-body">
-
-          {/* <!-- navbar start  --> */}
-            <Header />
-          {/* <!-- navbar end  --> */}
-
-          {/* <!-- body-content start/  --> */}
-          <div className="gs-content card m-3 p-3">
-
-      
-
-
-
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
-              Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
-              doloremque sint tempore unde debitis!</p>
-          </div>
-          {/* <!-- body-content end  --> */}
-
-
-
-
+          <Header />
+          <Home />
         </div>
       </div>
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,148 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <div className="gs-content card m-3 p-3">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eaque maxime pariatur atque!
+                    Similique recusandae expedita exercitationem nam in sed, ex consequatur, quo iusto vel
+                    doloremque sint tempore unde debitis!</p>
+            </div>
+
+        </>
+    )
+}
+
+export default Home
